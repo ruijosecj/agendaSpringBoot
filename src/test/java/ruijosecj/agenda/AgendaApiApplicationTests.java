ae2d978e708d5df63ae52f3e91499c27860497f5
ruijosecj <ruijosecj@gmail.com>
2023-04-06 13:54:07 -0300

    Criacao do projeto spring agenda

---
+++ src/test/java/ruijosecj/agenda/AgendaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ruijosecj.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
